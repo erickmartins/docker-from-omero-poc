82c366190545c46e1ec7efc7a47749ac07a4e4c0
Erick Martins Ratamero <erickmartins@gmx.net>
2021-12-20 10:41:01 -0500

    parsing and posting annotation

--- test_script.py
+++ test_script.py
@@ -1,15 +1,23 @@
 import ezomero
 import argparse
+import json
 from omero.clients import BaseClient
 from omero.gateway import BlitzGateway
 
-def main(id):
+def main(id, kv):
     print("this is a test")
     print(id)
+    print(kv)
+    params = json.loads(kv)
+    print(params)
+    data_type = params['Data_Type']
+    data_id = params['IDs']
+    ann = {params['Key']:params['Value']}
     client = BaseClient(host='ctomerodev.jax.org')
     client.joinSession(id)
     conn = BlitzGateway(client_obj=client)
     ezomero.print_groups(conn)
+    ezomero.post_map_annotation(conn, data_type, data_id, ann, ns='test')
     conn.close()
 
 
@@ -19,6 +27,9 @@ if __name__ == "__main__":
     parser.add_argument('id',
                         type=str,
                         help='uuid of the session to be used')
+    parser.add_argument('kv',
+                        type=str,
+                        help='dictionary with kv pair and where to put it')
     args = parser.parse_args()
 
     main(args.id)
